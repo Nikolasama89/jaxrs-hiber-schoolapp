9c57ed31865a24560b50019d0b6a15a26719ff2d
Nikos <niqos89@gmail.com>
2025-05-08 21:17:23 +0300

    initial commit

---
+++ src/main/java/gr/aueb/cf/schoolapp/SchoolApplication.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.schoolapp;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+// το convention ειναι αυτο συνηθως
+@ApplicationPath("/api")
+public class SchoolApplication extends Application {
+}
