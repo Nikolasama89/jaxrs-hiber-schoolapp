8c8a9a52ee4abf5dec8a4596f3573a674a0e1eca
Nikos <niqos89@gmail.com>
2025-05-15 19:22:59 +0300

    feat: add teacher controller

---
+++ src/main/java/gr/aueb/cf/schoolapp/rest/TeacherRestController.java
@@ -0,0 +1,47 @@
+package gr.aueb.cf.schoolapp.rest;
+
+import gr.aueb.cf.schoolapp.core.exceptions.EntityAlreadyExistsException;
+import gr.aueb.cf.schoolapp.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.schoolapp.security.ITeacherService;
+import gr.aueb.cf.schoolapp.validator.ValidatorUtil;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+import lombok.RequiredArgsConstructor;
+
+import java.net.URI;
+import java.util.List;
+
+@ApplicationScoped
+@RequiredArgsConstructor(onConstructor_ = @__(@Inject))     // ME LOMBOK
+@Path("/teachers")// ME LOMBOK
+public class TeacherRestController {
+
+    private final ITeacherService teacherService;
+
+    // ΧΩΡΙΣ LOMBOK
+//    @Inject
+//    public TeacherRestController(ITeacherService teacherService) {
+//        this.teacherService = teacherService;
+//    }
+
+    public Response addTeacher(TeacherInsertDTO insertDTO, @Context UriInfo uriInfo) throws EntityInvalidArgumentException, EntityAlreadyExistsException {
+        List<String> errors = ValidatorUtil.validateDTO(insertDTO);
+        if (!errors.isEmpty()) {
+            throw new EntityInvalidArgumentException("Teacher", String.join("\n", errors));
+        }
+
+        TeacherReadOnlyDTO readOnlyDTO = teacherService.insertTeacher(insertDTO);
+        URI newResourceUri = uriInfo.getAbsolutePathBuilder()
+                .path(String.valueOf(readOnlyDTO.id()))
+                .build();
+
+        return Response.created(newResourceUri).entity(readOnlyDTO).build();
+
+    }
+}
